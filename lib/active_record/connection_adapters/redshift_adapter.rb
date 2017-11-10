76f9600a731075cbb3dbb74b0b4de6265cad3a04
Thomas Lecavelier <ook@ookook.fr>
2017-11-10 09:42:10 +0100

    followanalytics/fa-issues#2612 Redshift doesn't support DISCARD ALL statement (introduced after RS PG fork)

--- lib/active_record/connection_adapters/redshift_adapter.rb
+++ lib/active_record/connection_adapters/redshift_adapter.rb
@@ -283,7 +283,7 @@ module ActiveRecord
         unless @connection.transaction_status == ::PG::PQTRANS_IDLE
           @connection.query 'ROLLBACK'
         end
-        @connection.query 'DISCARD ALL'
+        @connection.query 'RESET ALL'
         configure_connection
       end
 
